37f4068ee23c57df4db540caa29ea37cb43d5d59
sakellar <angsakel@hotmail.com>
2018-07-16 01:12:41 +0300

    Update db_utils.py

--- app/src/db_utils.py
+++ app/src/db_utils.py
@@ -1,1 +1,19 @@
+import mysql.connector
 
+class DB:
+    def connect(self):
+        raise NotImplementedError
+    def update(self, dictionary):
+        raise NotImplementedError
+    def close(self):
+        raise NotImplementedError
+
+class WordDB:
+  def __init__(self):
+      self.table_exists = False
+  def connect(self):
+      pass
+  def update(self, dictionary):
+      pass
+  def close(self):
+      pass
